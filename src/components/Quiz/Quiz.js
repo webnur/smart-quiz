21a0b769f0db0cda7b6286bf6daf6ee2fa693bb0
webnur <mdnuruli291@gmai.com>
2022-10-12 11:36:27 +0600

    quizzes page design and responisve alhamdulillah

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Option from '../Option/Option';
 import './Quiz.css'
+import { EyeIcon } from '@heroicons/react/24/solid'
+import { toast } from 'react-toastify';
 
 const Quiz = ({quiz}) => {
     // console.log(quiz)
     const {correctAnswer, id, options, question} = quiz;
+
+    const handleCorrectAnswer = () => {
+        toast.success(correctAnswer, { autoClose: 1000 })
+    }
     return (
         <div>
-            <h2>{question}</h2>
-            <p>{correctAnswer}</p>
+            <div className='question-area'>
+                <h5>{question}</h5>
+                <EyeIcon className='icon' onClick={handleCorrectAnswer}></EyeIcon>
+            </div>
             {
                 options.map((option, index )=> <Option key={index} option={option} correctAnswer={correctAnswer}></Option>)
             }
--- src/components/Quizzes/Quizzes.js
+++ src/components/Quizzes/Quizzes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
 
@@ -7,11 +8,14 @@ const Quizzes = () => {
     const quizzes = data.data.questions;
     // console.log(quizzes)
     return (
-        <div>
-           {
-            quizzes.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
-           }
-        </div>
+            <Container className='my-5'>
+                <Row xs={1} md={1} lg={1} className="g-4">
+                    {
+                        quizzes.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
+                    }
+                </Row>
+                
+            </Container>
     );
 };
 
